a9dcf20c6a43d5137ff9fbfd5a1ed413ae356a7d
Ignacio <shoik.xtazy@gmail.com>
2019-08-14 23:31:48 -0400

    change indexjs to class and render

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {render} from 'react-dom';
 
+class App extends Component{
+    render(){
+        return(
+            <div>
+                <nav className="navbar navbar-light bg-primary">
+                    <a className="navbar-brand text-white">Chat React</a>
+                </nav>
+            </div>
+        )
+    }
+}
 render(
-    <h1>Hello world 2</h1>,
+    <App/>,
     document.getElementById('app')
 ); 
